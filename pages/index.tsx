0c5d9dbf2b97dd9db7920fbbb0db8eca0f592f58
John Victor Stephen Durairaj <jh312868@dal.ca>
2024-08-18 17:28:29 -0300

    changed UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Save() {
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0.8 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
-      className="h-screen w-screen flex items-center justify-center flex-col bg-gradient-to-r from-purple-600 via-blue-500 to-pink-500"
+      className="h-screen w-screen flex items-center justify-center flex-col bg-black"
     >
       <motion.img
         src="/icon512_maskable.png"
@@ -28,24 +28,31 @@ export default function Save() {
         whileHover={{ scale: 1.1 }}
         transition={{ duration: 0.3 }}
       />
-      <h1 className="text-white text-5xl mt-6 font-extrabold drop-shadow-lg">
+      <h1
+        className="text-5xl mt-6 font-semibold text-center text-transparent bg-clip-text"
+        style={{
+          backgroundImage: "linear-gradient(45deg, #fff, #ffecb3)",
+          WebkitTextFillColor: "transparent",
+          WebkitBackgroundClip: "text",
+        }}
+      >
         Your Eyes
       </h1>
-      <p className="text-white text-xl mt-4 text-center font-light drop-shadow-md max-w-xl">
+      <p className="text-gray-400 text-xl mt-4 text-center max-w-xl">
         Real-time audio descriptions for the visually impaired, fostering
         independence and safety.
       </p>
 
       <div className="flex flex-col items-center justify-center mt-6">
-        <div className="text-white text-sm text-center p-4 bg-white/10 rounded-lg backdrop-blur-lg max-w-lg shadow-md">
+        <div className="text-gray-300 text-sm text-center p-4 bg-gray-800 rounded-lg max-w-lg shadow-md">
           We recommend saving this app to your home screen for the best
-          experience. Saved apps will launch visionarypath automatically.
+          experience. Saved apps will launch Your Eyes automatically.
           Alternatively, you can use the app in your browser by clicking the
           button below.
         </div>
         <a
           href="/app"
-          className="bg-white text-black px-4 py-2 rounded-full mt-6 font-mono shadow-lg hover:bg-black hover:text-white transition-all duration-300 ease-in-out"
+          className="bg-gradient-to-r from-gray-700 to-gray-500 text-white px-4 py-2 rounded-full mt-6 font-mono shadow-lg hover:from-gray-600 hover:to-gray-400 transition-all duration-300 ease-in-out"
         >
           Start seeing
         </a>
