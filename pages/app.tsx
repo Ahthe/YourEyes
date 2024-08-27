ca12f5e735b66efb4f964c19ec25d87a9038311f
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-08-27 13:43:59 -0400

    optimized the audio performace, integrated lazy loading for smoother performace

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,9 +1,15 @@
 import Yolo from "@/components/models/yolo";
+import dynamic from "next/dynamic";
 import "@/controllers/init";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const LazyYolo = dynamic(() => import("@/components/models/yolo"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false
+});
+
 export default function Home() {
-  return <Yolo />;
+  return <LazyYolo />;
 }
